bfa788a977a7b7859abff968f547bebc2c12c6ac
Alison <alisonhmullin@gmail.com>
2016-08-01 19:52:29 -0400

    build classes with objects that relate to one another

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+class Backer
+
+attr_reader :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project_object)
+    backed_projects << project_object
+    project_object.backers << self
+  end
+
+end
